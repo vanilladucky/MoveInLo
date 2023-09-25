90f6f051c870a34fa2936059273c1085cdee9687
iujinwee <eugeneftw500@gmail.com>
2023-09-25 16:44:16 +0800

    Dev: Updated landing page styling

--- MoveInLo/src/app/index.js
+++ MoveInLo/src/app/index.js
@@ -1,35 +1,50 @@
 import React from "react";
 import BaseButton from "@src/components/utils/button";
 import ThemeProvider, { useTheme } from "@src/assets/theme/ThemeProvider";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Screen } from "react-native-screens";
-import { useRouter } from "expo-router";
+import LandingIcon from "@src/assets/splash/LandingLogo.png";
 
 const App = () => {
   const { theme } = useTheme();
-
-  const router = useRouter();
 
   const styles = StyleSheet.create({
     background: {
       backgroundColor: theme.background,
     },
+    font: {
+      fontFamily: "RobotoBold",
+      fontSize: 20,
+      textAlign: "center",
+      padding: 15,
+    },
     container: {
       width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 20,
+    },
+    buttons: {
       display: "flex",
       flexDirection: "row",
       gap: 20,
-      alignItems: "center",
-      justifyContent: "center",
     },
   });
 
   return (
     <ThemeProvider>
       <SafeAreaProvider>
-        <Screen style={styles.background}>
-          <View style={styles.container}>
+        <Screen style={[styles.background, styles.container, styles.font]}>
+          <View>
+            <Text style={styles.font}>Welcome to</Text>
+            <Image source={LandingIcon} />
+            <Text style={styles.font}>MoveInLo!</Text>
+          </View>
+          <View style={styles.buttons}>
             <BaseButton title="Login" link="/auth/login" />
             <BaseButton title="Sign up" link="/auth/signup" />
           </View>
