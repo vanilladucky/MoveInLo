58f9d5d0edd058dcf89207708f169bec177f63f6
iujinwee <eugeneftw500@gmail.com>
2023-10-10 01:04:38 +0800

    Added initial code for signup

--- MoveInLo/src/app/auth/signup.js
+++ MoveInLo/src/app/auth/signup.js
@@ -1,23 +1,109 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { useRouter } from "expo-router";
-import { Input } from "native-base";
 import BaseButton from "@src/components/utils/button";
+import BaseInput from "@src/components/utils/inputbox";
+import {
+  CheckIcon,
+  FormControl,
+  Select,
+  WarningOutlineIcon,
+} from "native-base";
+import { useState } from "react";
+import ErrorAlert from "@src/components/utils/erroralert";
 
 const SignUpUI = () => {
+  const [newAccountInfo, setNewAccountInfo] = useState({
+    email: "",
+    username: "",
+    password: "",
+    age: "",
+    type: "",
+    number: "",
+  });
+  const [invalidInput, setInvalidInput] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   const router = useRouter();
 
+  const inputHandler = (input, field) => {
+    setNewAccountInfo(() => ({ ...newAccountInfo, [field]: input }));
+  };
+
+  const invalidHandler = (bool, field) => {
+    setInvalidInput(() => ({ ...newAccountInfo, [field]: bool }));
+  };
+
+  const resetHandler = () => {
+    newAccountInfo.type = "";
+    newAccountInfo.username = "";
+    newAccountInfo.password = "";
+    newAccountInfo.email = "";
+    newAccountInfo.age = "";
+    newAccountInfo.number = "";
+    setShowAlert(false);
+  };
+
+  const isValidInput = () => {
+    // TODO: Insert backend logic
+    const validType = newAccountInfo.type !== "";
+    const validEmail =
+      newAccountInfo.email !== "" &&
+      newAccountInfo.email.includes("@") &&
+      newAccountInfo.email.includes(".com");
+    const validUsername = newAccountInfo.username !== "";
+    const validPassword =
+      newAccountInfo.password !== "" &&
+      newAccountInfo.password.includes(
+        "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$\n"
+      );
+    const validAge = newAccountInfo.age >= 18 && newAccountInfo.age < 100;
+    const validNumber = newAccountInfo.number.length === 8;
+
+    return (
+      validType &&
+      validEmail &&
+      validUsername &&
+      validPassword &&
+      validAge &&
+      validNumber
+    );
+  };
+
+  const submitHandler = () => {
+    invalidHandler(!newAccountInfo.username, "username");
+    invalidHandler(!newAccountInfo.password, "password");
+    invalidHandler(!newAccountInfo.type, "type");
+    console.log(newAccountInfo);
+
+    if (isValidInput()) {
+      router.push("/customer/home");
+    } else {
+      setShowAlert(true);
+    }
+  };
+
   return (
-    <View className={"flex h-full w-full items-center mt-10"}>
-      <View className={"flex flex-row"}>
-        <View className={"border-r-2"}>
-          <Text
-            className="font-RobotoBold text-xl my-2 w-40 text-center"
-            onPress={() => router.push("/auth/login")}
-          >
+    <View className={"flex h-full w-full items-center"}>
+      {!isValidInput() && (
+        <View className={"absolute z-10 w-3/4"}>
+          <ErrorAlert
+            title={"Please try again!"}
+            message={"You have missing or invalid inputs!"}
+            onPress={() => resetHandler()}
+            shown={showAlert}
+          />
+        </View>
+      )}
+      <View className={"flex flex-row mt-20"}>
+        <Pressable
+          onPress={() => {
+            router.push("/auth/login");
+          }}
+        >
+          <Text className="font-RobotoBold text-xl my-2 w-40 text-center">
             Login
           </Text>
-        </View>
-        <View className={"border-r-2 flex items-center"}>
+        </Pressable>
+        <View className={"border-l-2 flex items-center"}>
           <Text className="font-RobotoBold text-xl my-2 w-40 text-center">
             Sign Up
           </Text>
@@ -25,32 +111,77 @@ const SignUpUI = () => {
         </View>
       </View>
 
-      <View className={"flex flex-col w-3/4 mt-8"}>
-        <Input placeholder={"Email"} />
-        <Input placeholder={"Username"} />
-        <Input placeholder={"Age"} />
-        <Input placeholder={"Phone Number"} />
-        <Input placeholder={"Password"} />
-        <Input placeholder={"Re-enter Password"} />
-        <View className={"mt-4 p"}>
-          <Text className={"font-RobotoMedium"}>Note:</Text>
-          <Text className={"font-RobotoRegular"}>
-            Remember to keep your personal information safe and do not share it
-            with anyone.
+      <View className={"flex flex-col w-3/4 mt-4"}>
+        <BaseInput title={"Email"} placeholder={"Enter your email"} />
+
+        <BaseInput title={"Username"} placeholder={"Enter a username"} />
+
+        <View className={"flex flex-row mt-2 space-x-4"}>
+          <FormControl className={"w-1/2"}>
+            <Text className={"font-RobotoMedium"}>Account Type</Text>
+            <Select
+              selectedValue={newAccountInfo.type}
+              accessibilityLabel={"Select"}
+              placeholder={"Select"}
+              onValueChange={(itemValue) => inputHandler(itemValue, "type")}
+              _selectedItem={{
+                endIcon: <CheckIcon size="3" />,
+              }}
+            >
+              <Select.Item label={"Customer"} value={"customer"} />
+              <Select.Item label={"Job Seeker"} value={"jobseeker"} />
+            </Select>
+            {invalidInput.type && (
+              <FormControl.ErrorMessage
+                className={"mt-0 mb-2"}
+                leftIcon={<WarningOutlineIcon size="xs" />}
+              >
+                <Text className={"text-red-600"}>Please make a selection!</Text>
+              </FormControl.ErrorMessage>
+            )}
+          </FormControl>
+          <View className={"-mt-2.5 w-36"}>
+            <BaseInput
+              title={"Age"}
+              placeholder={"Enter your age"}
+              width={"full"}
+            />
+          </View>
+        </View>
+
+        <BaseInput
+          title={"Phone Number"}
+          placeholder={"Enter your phone number"}
+        />
+
+        <BaseInput title={"Password"} placeholder={"Enter your password"} />
+
+        <BaseInput
+          title={"Re-enter password"}
+          placeholder={"Re-enter your password"}
+        />
+
+        <View className={"mt-4"}>
+          <Text className={"font-RobotoMedium"}>
+            Note:{" "}
+            <Text className={"font-RobotoRegular"}>
+              Remember to keep your personal information safe and do not share
+              it with anyone.
+            </Text>
           </Text>
         </View>
-        <View className={"mt-6"}>
-          <BaseButton
-            primary
-            title={"Login"}
-            link={"customer/home"}
-            width={"full"}
-          />
-        </View>
-        <BaseButton title={"Back"} link={"/"} />
-      </View>
 
-      <BaseButton title={"Forget Password"} link={"auth/forgetpassword"} />
+        <View className={"mt-6"}>
+          <View>
+            <BaseButton
+              primary
+              title={"Sign up"}
+              onPress={() => submitHandler()}
+              width={"full"}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
